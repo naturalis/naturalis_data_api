ad581cdef832b2721d3ba5d3cefa426cfa597e8f
Ayco Holleman <ayco.holleman@naturalis.nl>
2014-04-29 06:16:59 +0000

    NDA-1

---
+++ nl.naturalis.nda.ejb/src/main/java/nl/naturalis/nda/ejb/service/SpecimenService.java
@@ -0,0 +1,17 @@
+package nl.naturalis.nda.ejb.service;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Stateless
+@LocalBean
+public class SpecimenService {
+	
+	@SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory.getLogger(SpecimenService.class);
+
+}

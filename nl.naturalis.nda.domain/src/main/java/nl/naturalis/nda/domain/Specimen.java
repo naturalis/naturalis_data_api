599535667907bb4d78864ad35e6a4dca9c7987e7
Ayco Holleman <ayco.holleman@naturalis.nl>
2014-04-29 06:16:36 +0000

    NDA-1

---
+++ nl.naturalis.nda.domain/src/main/java/nl/naturalis/nda/domain/Specimen.java
@@ -0,0 +1,19 @@
+package nl.naturalis.nda.domain;
+
+public class Specimen {
+
+	private String barcode;
+
+
+	public String getBarcode()
+	{
+		return barcode;
+	}
+
+
+	public void setBarcode(String barcode)
+	{
+		this.barcode = barcode;
+	}
+
+}

ea0857d19461a03cc181d98deeed79737eff20b8
Ayco Holleman <ayco.holleman@naturalis.nl>
2014-11-12 13:44:52 +0000

    added recordURI to transfer

--- nl.naturalis.nda.elasticsearch.dao/src/main/java/nl/naturalis/nda/elasticsearch/dao/transfer/TaxonTransfer.java
+++ nl.naturalis.nda.elasticsearch.dao/src/main/java/nl/naturalis/nda/elasticsearch/dao/transfer/TaxonTransfer.java
@@ -5,25 +5,28 @@ import nl.naturalis.nda.elasticsearch.dao.estypes.ESTaxon;
 
 public class TaxonTransfer {
 
-    private TaxonTransfer() {
-        // Only static method in transfer objects
-    }
+	private TaxonTransfer()
+	{
+		// Only static method in transfer objects
+	}
 
-    public static Taxon transfer(ESTaxon esTaxon) {
-        Taxon taxon = new Taxon();
-        taxon.setSourceSystem(esTaxon.getSourceSystem());
-        taxon.setSourceSystemId(esTaxon.getSourceSystemId());
-        taxon.setSourceSystemParentId(esTaxon.getSourceSystemParentId());
-        taxon.setRecordURI(esTaxon.getRecordURI());
-        taxon.setTaxonRank(esTaxon.getTaxonRank());
-        taxon.setAcceptedName(esTaxon.getAcceptedName());
-        taxon.setDefaultClassification(esTaxon.getDefaultClassification());
-        taxon.setSystemClassification(esTaxon.getSystemClassification());
-        taxon.setSynonyms(esTaxon.getSynonyms());
-        taxon.setVernacularNames(esTaxon.getVernacularNames());
-        taxon.setDescriptions(esTaxon.getDescriptions());
-        taxon.setReferences(esTaxon.getReferences());
-        taxon.setExperts(esTaxon.getExperts());
-        return taxon;
-    }
+
+	public static Taxon transfer(ESTaxon esTaxon)
+	{
+		Taxon taxon = new Taxon();
+		taxon.setSourceSystem(esTaxon.getSourceSystem());
+		taxon.setSourceSystemId(esTaxon.getSourceSystemId());
+		taxon.setSourceSystemParentId(esTaxon.getSourceSystemParentId());
+		taxon.setRecordURI(esTaxon.getRecordURI());
+		taxon.setTaxonRank(esTaxon.getTaxonRank());
+		taxon.setAcceptedName(esTaxon.getAcceptedName());
+		taxon.setDefaultClassification(esTaxon.getDefaultClassification());
+		taxon.setSystemClassification(esTaxon.getSystemClassification());
+		taxon.setSynonyms(esTaxon.getSynonyms());
+		taxon.setVernacularNames(esTaxon.getVernacularNames());
+		taxon.setDescriptions(esTaxon.getDescriptions());
+		taxon.setReferences(esTaxon.getReferences());
+		taxon.setExperts(esTaxon.getExperts());
+		return taxon;
+	}
 }

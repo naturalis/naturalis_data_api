9c6f93592d409ef9135ddd9b79f51be43113e323
Reinier.Kartowikromo <Reinier.Kartowikromo@DT004106.nnm.local>
2015-01-13 13:50:57 +0100

    Add new classes to domain: Biostratigraphy Chronostratigraphy LitoStratigraphy

---
+++ nl.naturalis.nda.domain/src/main/java/nl/naturalis/nda/domain/BioStratigraphy.java
@@ -0,0 +1,170 @@
+package nl.naturalis.nda.domain;
+
+public class BioStratigraphy
+{
+	private String youngBioDatingQualifier;
+	private String youngBioName;
+	private String youngFossilZone;
+	private String youngFossilSubZone;
+	private String youngBioCertainty;
+	private String youngStratType;
+	private String bioDatingQualifier;
+	private boolean bioPreferredFlag;
+	private String rangePosition;
+	private String oldBioName;
+	private String bioIdentifier;
+	private String oldFossilzone;
+	private String oldFossilSubzone;
+	private String oldBioCertainty;
+	private String oldBioStratType;
+	
+	public String getYoungBioDatingQualifier()
+	{
+		return youngBioDatingQualifier;
+	}
+	
+	public void setYoungBioDatingQualifier(String youngBioDatingQualifier)
+	{
+		this.youngBioDatingQualifier = youngBioDatingQualifier;
+	}
+
+	public String getYoungBioName()
+	{
+		return youngBioName;
+	}
+
+	public void setYoungBioName(String youngBioName)
+	{
+		this.youngBioName = youngBioName;
+	}
+
+	public String getYoungFossilZone()
+	{
+		return youngFossilZone;
+	}
+
+	public void setYoungFossilZone(String youngFossilZone)
+	{
+		this.youngFossilZone = youngFossilZone;
+	}
+
+	public String getYoungFossilSubZone()
+	{
+		return youngFossilSubZone;
+	}
+
+	public void setYoungFossilSubZone(String youngFossilSubZone)
+	{
+		this.youngFossilSubZone = youngFossilSubZone;
+	}
+
+	public String getYoungBioCertainty()
+	{
+		return youngBioCertainty;
+	}
+
+	public void setYoungBioCertainty(String youngBioCertainty)
+	{
+		this.youngBioCertainty = youngBioCertainty;
+	}
+
+	public String getYoungStratType()
+	{
+		return youngStratType;
+	}
+
+	public void setYoungStratType(String youngStratType)
+	{
+		this.youngStratType = youngStratType;
+	}
+
+	public String getBioDatingQualifier()
+	{
+		return bioDatingQualifier;
+	}
+
+	public void setBioDatingQualifier(String bioDatingQualifier)
+	{
+		this.bioDatingQualifier = bioDatingQualifier;
+	}
+
+	public boolean isBioPreferredFlag()
+	{
+		return bioPreferredFlag;
+	}
+
+	public void setBioPreferredFlag(boolean bioPreferredFlag)
+	{
+		this.bioPreferredFlag = bioPreferredFlag;
+	}
+
+	public String getRangePosition()
+	{
+		return rangePosition;
+	}
+
+	public void setRangePosition(String rangePosition)
+	{
+		this.rangePosition = rangePosition;
+	}
+
+	public String getOldBioName()
+	{
+		return oldBioName;
+	}
+
+	public void setOldBioName(String oldBioName)
+	{
+		this.oldBioName = oldBioName;
+	}
+
+	public String getBioIdentifier()
+	{
+		return bioIdentifier;
+	}
+
+	public void setBioIdentifier(String bioIdentifier)
+	{
+		this.bioIdentifier = bioIdentifier;
+	}
+
+	public String getOldFossilzone()
+	{
+		return oldFossilzone;
+	}
+
+	public void setOldFossilzone(String oldFossilzone)
+	{
+		this.oldFossilzone = oldFossilzone;
+	}
+
+	public String getOldFossilSubzone()
+	{
+		return oldFossilSubzone;
+	}
+
+	public void setOldFossilSubzone(String oldFossilSubzone)
+	{
+		this.oldFossilSubzone = oldFossilSubzone;
+	}
+
+	public String getOldBioCertainty()
+	{
+		return oldBioCertainty;
+	}
+
+	public void setOldBioCertainty(String oldBioCertainty)
+	{
+		this.oldBioCertainty = oldBioCertainty;
+	}
+
+	public String getOldBioStratType()
+	{
+		return oldBioStratType;
+	}
+
+	public void setOldBioStratType(String oldBioStratType)
+	{
+		this.oldBioStratType = oldBioStratType;
+	}
+}

512f70aa9a66a1e421484a52f8c24942b0a2e0cd
rkartowikromo <Reinier.kartowikromo@naturalis.nl>
2015-02-05 08:52:03 +0100

    DwCA Export

--- nl.naturalis.nda.export/src/main/java/nl/naturalis/nda/export/dwca/DwCAExporter.java
+++ nl.naturalis.nda.export/src/main/java/nl/naturalis/nda/export/dwca/DwCAExporter.java
@@ -6,7 +6,19 @@
  */
 package nl.naturalis.nda.export.dwca;
 
+
+
+import java.io.IOException;
+
+import nl.naturalis.nda.elasticsearch.client.IndexNative;
+import nl.naturalis.nda.elasticsearch.dao.estypes.ESSpecimen;
 import nl.naturalis.nda.elasticsearch.load.LoadUtil;
+import nl.naturalis.nda.elasticsearch.load.NDAIndexManager;
+import static nl.naturalis.nda.elasticsearch.load.NDAIndexManager.LUCENE_TYPE_SPECIMEN;
+
+
+
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,22 +30,46 @@ import org.slf4j.LoggerFactory;
 public class DwCAExporter
 {
 	static final Logger logger = LoggerFactory.getLogger(DwCAExporter.class);
+
 	
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
+		
 		logger.info("-----------------------------------------------------------------");
 		logger.info("Start");
-		
-		
+		IndexNative index = new IndexNative(LoadUtil.getESClient(), LoadUtil.getConfig().required("elasticsearch.index.name"));
+		try
+		{
+			DwCAExporter exp = new DwCAExporter(index);
+			exp.ExportDwca();
+		} finally
+		{
+			index.getClient().close();
+		}
 		
 		
 		logger.info("Ready");
-		
-		LoadUtil.getESClient();
-
 	}
+	
+	
+	
+	public DwCAExporter(IndexNative index)
+	{
+		this.index = index;
+	}
+	
+	private final IndexNative index;
+
+
+	public void ExportDwca() throws IOException
+	{
+		index.getResultsList(NDAIndexManager.LUCENE_TYPE_SPECIMEN, 25);//, ESSpecimen.class);
+	}
+
+	
 
 }

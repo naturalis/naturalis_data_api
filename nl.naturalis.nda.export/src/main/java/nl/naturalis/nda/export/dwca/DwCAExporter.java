f8c1ea0f4abcff6550cebdaa7ce7e386f2929445
rkartowikromo <Reinier.kartowikromo@naturalis.nl>
2015-02-12 16:29:49 +0100

    XML Creator

--- nl.naturalis.nda.export/src/main/java/nl/naturalis/nda/export/dwca/DwCAExporter.java
+++ nl.naturalis.nda.export/src/main/java/nl/naturalis/nda/export/dwca/DwCAExporter.java
@@ -1,4 +1,4 @@
-/**
+/*
  *  Description: Create a new DwCA export tool for the NBA
  *  Date: January 28th 2015
  *  Developer: Reinier Kartowikromo
@@ -9,28 +9,24 @@ package nl.naturalis.nda.export.dwca;
 import static nl.naturalis.nda.elasticsearch.load.NDAIndexManager.LUCENE_TYPE_SPECIMEN;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.lang.Object;
-import java.util.Arrays;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 import nl.naturalis.nda.elasticsearch.client.IndexNative;
 import nl.naturalis.nda.elasticsearch.dao.estypes.ESSpecimen;
 import nl.naturalis.nda.elasticsearch.dao.estypes.ESGatheringEvent;
 import nl.naturalis.nda.elasticsearch.load.LoadUtil;
-import nl.naturalis.nda.export.dwca.CSVWriter;
 
 import org.domainobject.util.debug.BeanPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sun.istack.internal.Nullable;
+
 
 /**
  * @author Reinier.Kartowikromo
@@ -40,7 +36,8 @@ public class DwCAExporter
 {
 	static final Logger logger = LoggerFactory.getLogger(DwCAExporter.class);
 	public static final String CSVComma = "\t";
-	static final String csvOutPutFile = "specimen.txt";
+	private static final String csvOutPutFile = "specimen.txt";
+	private static final String FILE_NAME = "meta.xml";
 
 	/**
 	 * @param args
@@ -80,46 +77,10 @@ public class DwCAExporter
 	public void ExportDwca() throws IOException
 	{
 		// before we open the file check to see if it already exists
-		boolean alreadyExists = new File(csvOutPutFile).exists();
+		// boolean alreadyExists = new File(csvOutPutFile).exists();
 		CsvFileWriter fileWriter = new CsvFileWriter(csvOutPutFile);
-
-		//FileOutputStream fos = new FileOutputStream(csvOutPutFile);
-		//PrintWriter pw = new PrintWriter(fos, true);
-		//CSVWriter csv = new CSVWriter(pw, false, '\t', System.getProperty("line.separator"));
-		//csv.writeCommentln("Specimen csv-file");
-	//	csv.writeCommentln("Created by Reinier.Kartowikromo");
-
 		List<ESSpecimen> list = index.getResultsList(LUCENE_TYPE_SPECIMEN, 10, ESSpecimen.class);
 		BeanPrinter.out(list);
-
-//		if (!alreadyExists)
-//		{
-//			csv.write("UnitID");
-//			csv.write("UnitGUID");
-//			csv.write("CollectorsFieldNumber");
-//			csv.write("AssemblageID");
-//			csv.write("SourceInstitutionID");
-//			csv.write("SourceID");
-//			csv.write("Owner");
-//			csv.write("LicenceType");
-//			csv.write("Licence");
-//			csv.write("RecordBasis");
-//			csv.write("KindOfUnit");
-//			csv.write("CollectionType");
-//			csv.write("TypeStatus");
-//			csv.write("Sex");
-//			csv.write("PhaseOrStage");
-//			csv.write("Titles");
-//			csv.write("Notes");
-//			csv.write("PreparationType");
-//			csv.write("NumberOfSpecimen");
-//			csv.write("FromCaptivity");
-//			csv.write("ObjectPublic");
-//			csv.write("MultiMediaPublic");
-//			csv.write("AcquiredFrom");
-//			csv.write("ProjectTitle");
-//			csv.writeln();
-//		}
 
 		CsvFileWriter.CsvRow headerRow = fileWriter.new CsvRow();
 		headerRow.add("UnitID");
@@ -154,10 +115,9 @@ public class DwCAExporter
 		 */
 		fileWriter.WriteRow(headerRow);
 
-
 		for (ESSpecimen specimen : list)
 		{
-			//pw.println(getCSVRecordFromSpecimen(specimen));
+			// pw.println(getCSVRecordFromSpecimen(specimen));
 
 			CsvFileWriter.CsvRow dataRow = fileWriter.new CsvRow();
 			dataRow.add(specimen.getUnitID());
@@ -188,41 +148,91 @@ public class DwCAExporter
 			dataRow.add(specimen.getGatheringEvent().getCountry());
 
 			/**
-			 * adding data row 
+			 * adding data row
 			 */
 			fileWriter.WriteRow(dataRow);
+			
+			XMLSpecimen xmlspecimen = new XMLSpecimen();
+			xmlspecimen.setKindOfUnit(specimen.getKindOfUnit());
+			xmlspecimen.setOwner(specimen.getOwner());
+	        DwCAObjectToXML(xmlspecimen);
+	 
+	        XMLSpecimen specFromFile = DwCAXMLToObject();
+	        System.out.println(specFromFile.toString());
 		}
 
 		/* always close the csv writer object after use */
 		fileWriter.close();
-		//pw.close();
+		// pw.close();
+		
+		
 	}
 
-//	private String getCSVRecordFromSpecimen(ESSpecimen specimen) throws IOException
-//	{
-//		String result = null;
-//		result = specimen.getUnitID() + CSVComma + specimen.getUnitGUID() + CSVComma
-//				+ specimen.getCollectorsFieldNumber() + CSVComma + specimen.getAssemblageID() + CSVComma
-//				+ specimen.getSourceInstitutionID() + CSVComma + specimen.getSourceID() + CSVComma
-//				+ specimen.getOwner() + CSVComma + specimen.getLicenceType() + CSVComma
-//				+ specimen.getLicence() + CSVComma + specimen.getRecordBasis() + CSVComma
-//				+ specimen.getKindOfUnit() + CSVComma + specimen.getCollectionType() + CSVComma
-//				+ specimen.getTypeStatus() + CSVComma + specimen.getSex() + CSVComma
-//				+ specimen.getPhaseOrStage() + CSVComma + specimen.getTitle() + CSVComma
-//				+ specimen.getNotes() + CSVComma + specimen.getPreparationType() + CSVComma
-//				+ specimen.getNumberOfSpecimen() + CSVComma + String.valueOf(specimen.isFromCaptivity())
-//				+ CSVComma + Boolean.toString(specimen.isObjectPublic()) + CSVComma
-//				+ Boolean.toString(specimen.isMultiMediaPublic()) + CSVComma
-//				+ specimen.getGatheringEvent().getProjectTitle();
-//
-//		// ArrayList<String> list = new ArrayList<String>();
-//		// list.add(ESGatheringEvent.class.toString());
-//		// for (String each : list)
-//		// {
-//		// resGathering = each + "\t";
-//		//
-//		// }
-//		return result;
-//	}
+	private static XMLSpecimen DwCAXMLToObject()
+	{
+		try
+		{
+			JAXBContext context = JAXBContext.newInstance(XMLSpecimen.class);
+			Unmarshaller un = context.createUnmarshaller();
+			XMLSpecimen xmlspecimen = (XMLSpecimen) un.unmarshal(new File(FILE_NAME));
+			return xmlspecimen;
+		} catch (JAXBException e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static void DwCAObjectToXML(XMLSpecimen specimen)
+	{
+
+		try
+		{
+			JAXBContext context = JAXBContext.newInstance(XMLSpecimen.class);
+			Marshaller m = context.createMarshaller();
+			// for pretty-print XML in JAXB
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+			// Write to File
+			m.marshal(specimen, new File(FILE_NAME));
+		} catch (JAXBException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	// private String getCSVRecordFromSpecimen(ESSpecimen specimen) throws
+	// IOException
+	// {
+	// String result = null;
+	// result = specimen.getUnitID() + CSVComma + specimen.getUnitGUID() +
+	// CSVComma
+	// + specimen.getCollectorsFieldNumber() + CSVComma +
+	// specimen.getAssemblageID() + CSVComma
+	// + specimen.getSourceInstitutionID() + CSVComma + specimen.getSourceID() +
+	// CSVComma
+	// + specimen.getOwner() + CSVComma + specimen.getLicenceType() + CSVComma
+	// + specimen.getLicence() + CSVComma + specimen.getRecordBasis() + CSVComma
+	// + specimen.getKindOfUnit() + CSVComma + specimen.getCollectionType() +
+	// CSVComma
+	// + specimen.getTypeStatus() + CSVComma + specimen.getSex() + CSVComma
+	// + specimen.getPhaseOrStage() + CSVComma + specimen.getTitle() + CSVComma
+	// + specimen.getNotes() + CSVComma + specimen.getPreparationType() +
+	// CSVComma
+	// + specimen.getNumberOfSpecimen() + CSVComma +
+	// String.valueOf(specimen.isFromCaptivity())
+	// + CSVComma + Boolean.toString(specimen.isObjectPublic()) + CSVComma
+	// + Boolean.toString(specimen.isMultiMediaPublic()) + CSVComma
+	// + specimen.getGatheringEvent().getProjectTitle();
+	//
+	// // ArrayList<String> list = new ArrayList<String>();
+	// // list.add(ESGatheringEvent.class.toString());
+	// // for (String each : list)
+	// // {
+	// // resGathering = each + "\t";
+	// //
+	// // }
+	// return result;
+	// }
 
 }

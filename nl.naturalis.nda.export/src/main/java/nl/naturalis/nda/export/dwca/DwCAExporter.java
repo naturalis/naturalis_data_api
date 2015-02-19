8d9c4f65ff00fecb226c58a00ea03c2bd887c22d
rkartowikromo <Reinier.kartowikromo@naturalis.nl>
2015-02-19 14:33:02 +0100

    Core Field index

--- nl.naturalis.nda.export/src/main/java/nl/naturalis/nda/export/dwca/DwCAExporter.java
+++ nl.naturalis.nda.export/src/main/java/nl/naturalis/nda/export/dwca/DwCAExporter.java
@@ -9,24 +9,25 @@ package nl.naturalis.nda.export.dwca;
 import static nl.naturalis.nda.elasticsearch.load.NDAIndexManager.LUCENE_TYPE_SPECIMEN;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.bind.annotation.*;
 
 import nl.naturalis.nda.elasticsearch.client.IndexNative;
 import nl.naturalis.nda.elasticsearch.dao.estypes.ESSpecimen;
-import nl.naturalis.nda.elasticsearch.dao.estypes.ESGatheringEvent;
 import nl.naturalis.nda.elasticsearch.load.LoadUtil;
+import nl.naturalis.nda.export.dwca.CsvFileWriter.CsvRow;
 import nl.naturalis.nda.export.dwca.Eml;
 
 import org.domainobject.util.debug.BeanPrinter;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,6 +42,7 @@ public class DwCAExporter
 	private static final String csvOutPutFile = "specimen.txt";
 	private static final String FILE_NAME = "meta.xml";
 	private static final String FILE_NAME_EML = "eml.xml";
+	private static final String dwcUrlTdwgOrg = "http://rs.tdwg.org/dwc/terms/";
 
 	/**
 	 * @param args
@@ -85,6 +87,7 @@ public class DwCAExporter
 		List<ESSpecimen> list = index.getResultsList(LUCENE_TYPE_SPECIMEN, 10, ESSpecimen.class);
 		BeanPrinter.out(list);
 
+		/* Create the CSV file */
 		CsvFileWriter.CsvRow headerRow = fileWriter.new CsvRow();
 		headerRow.add("UnitID");
 		headerRow.add("UnitGUID");
@@ -155,10 +158,102 @@ public class DwCAExporter
 			 */
 			fileWriter.WriteRow(dataRow);
 
+			Files files = new Files();
+			files.setLocation("occurence.txt");
+			Id id = new Id();
+			id.setIndex(0);
+
+			// field.setTerm(dwcUrlTdwgOrg + headerRow.toString());//
+			// specimen.getTitle());
+
+			// for (ListIterator<Integer> iter = numbers.listIterator();
+			// iter.hasNext(); ) {
+			// Integer number = iter.next();
+			// iter.add(number+1); // insert a number right before this
+			// }
+			//
+
+	
+			/*
+			 * Iterator<String> fieldIterator = headerRow.iterator(); while
+			 * (fieldIterator.hasNext()) { // field.add(fieldIterator.next());
+			 * field.setTerm(dwcUrlTdwgOrg + fieldIterator.next());
+			 * field.setFields("2", headerRow); arglist.add(dwcUrlTdwgOrg +
+			 * fieldIterator.next()); }
+			 */
+
+			List<String> arg = new ArrayList<String>();
+			Iterator<String> fieldIterator = headerRow.iterator();
+			while (fieldIterator.hasNext())
+			{
+				arg.add(dwcUrlTdwgOrg + fieldIterator.next());
+			}
+			/*Fields fld = new Fields("123", arg);
+			JAXBContext jc = JAXBContext.newInstance(Fields.class);
+			Marshaller marshaller = jc.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			marshaller.marshal(fld, new File("test.xml"));
+			marshaller.marshal(fld, System.out);
+           */
+			
+			
+			Core cores = new Core("2", arg);
+			cores.setEncoding("UTF-8");
+			cores.setFieldsEnclosedBy("'");
+			cores.setFieldsTerminatedBy("\t");
+			cores.setLinesTerminatedBy("\r\n");
+			cores.setIgnoreHeaderLines("1");
+			cores.setRowtype("http://rs.tdwg.org/dwc/terms/Occurrence");
+			cores.setFiles(files);
+			cores.setId(id);
+//			cores.setField(field);
+			
+			/*Iterator<String> fieldIter = headerRow.iterator();
+			while (fieldIter.hasNext())
+			{
+				Field field = new Field();
+				field.setIndex("0");
+
+				field.setFields("2", dwcUrlTdwgOrg + fieldIter.next());
+				cores.setField(field);
+			}*/
+
+			// List<String> fields = new ArrayList<String>();
+			// for (CsvRow i : Arrays.asList(headerRow))
+			// fields.addAll(i);
+
+			// Iterator<String> fieldIterator = headerRow.iterator();
+			// while (fieldIterator.hasNext())
+			// {
+			// fields.add(fieldIterator.next());
+			// field.setTerm(dwcUrlTdwgOrg + fieldIterator.next());
+			// System.out.println(fieldIterator.next());
+			//
+			// }
+
+			// StringBuilder builder = new StringBuilder();
+			// for (ListIterator<String> iter = headerRow.listIterator();
+			// iter.hasNext();)
+			// {
+			//
+			// String column = iter.next();
+			// if (column != null)
+			// {
+			// builder.append(column);
+			// field.setTerm(dwcUrlTdwgOrg + builder.toString());//
+			// column.toString());
+			// System.out.println(column);
+			// }
+			//
+			// cores.setField(field);
+			// }
+
 			/* Create Meta.xml file for NBA */
 			Meta xmlspecimen = new Meta();
-			xmlspecimen.setKindOfUnit(specimen.getKindOfUnit());
-			xmlspecimen.setOwner(specimen.getOwner());
+			xmlspecimen.setMetadata("eml.xml");
+			xmlspecimen.setXmlnsxsi("http://www.w3.org/2001/XMLSchema-instance");
+			xmlspecimen.setXmlnstdwg("http://rs.tdwg.org/dwc/text/");
+			xmlspecimen.add(cores);
 			DwCAObjectToXML(xmlspecimen);
 
 			Meta specFromFile = DwCAXMLToObject();
@@ -199,13 +294,12 @@ public class DwCAExporter
 			// Write to File
 			m.marshal(meta, new File(FILE_NAME));
 			m.marshal(meta, System.out);
-			
+
 		} catch (JAXBException e)
 		{
 			e.printStackTrace();
 		}
 	}
-
 
 	private static void CreateEmlObjectToXML()
 	{
@@ -219,7 +313,7 @@ public class DwCAExporter
 			IndividualName indi = new IndividualName();
 			indi.setGivenname("Reinier");
 			indi.setSurname("Kartowikromo");
-			
+
 			/* Element Role info for Creator */
 			Role rolecreator = new Role();
 			rolecreator.setPosition("Analist");
@@ -228,7 +322,7 @@ public class DwCAExporter
 			IndividualName individual = new IndividualName();
 			individual.setGivenname("Wilfred");
 			individual.setSurname("Gerritsen");
-			
+
 			/* Element Role info for Provider */
 			Role roleprovider = new Role();
 			roleprovider.setPosition("Lead Software Developer");
@@ -237,7 +331,7 @@ public class DwCAExporter
 			IndividualName individualprovider = new IndividualName();
 			individualprovider.setGivenname("Ayco");
 			individualprovider.setSurname("Holleman");
-			
+
 			/* Element info "address" */
 			Address address = new Address();
 			address.setDeliveryPoint("Darwinweg 2");
@@ -245,7 +339,7 @@ public class DwCAExporter
 			address.setStateProvince("South Holland");
 			address.setCountry("Netherlands");
 			address.setPostalCode("NL-2300RA");
-			
+
 			/* Element info "contact" */
 			Contact contact = new Contact();
 			contact.setOrganisation("Naturalis Biodiversity Center");
@@ -255,7 +349,7 @@ public class DwCAExporter
 			contact.setRole(role);
 			contact.setIndividualName(indi);
 			contact.setAddress(address);
-			
+
 			/* Element info "creator" */
 			Creator creator = new Creator();
 			creator.setOrganisation("Naturalis Biodiversity Center");
@@ -265,7 +359,7 @@ public class DwCAExporter
 			creator.setRole(rolecreator);
 			creator.setIndividualName(individual);
 			creator.setAddress(address);
-			
+
 			/* Element info "provider" */
 			Provider provider = new Provider();
 			provider.setOrganisation("Naturalis Biodiversity Center");
@@ -275,7 +369,7 @@ public class DwCAExporter
 			provider.setRole(roleprovider);
 			provider.setIndividualName(individualprovider);
 			provider.setAddress(address);
-			
+
 			/* Element info "dataset" */
 			Dataset ds = new Dataset();
 			ds.setTitle("Naturalis Biodiversity Center(NL)");
@@ -287,7 +381,7 @@ public class DwCAExporter
 			ds.setContacts(contact);
 			ds.setCreator(creator);
 			ds.setProvider(provider);
-			
+
 			/* Header info Element "eml" */
 			Eml eml = new Eml();
 			eml.add(ds);
@@ -304,7 +398,7 @@ public class DwCAExporter
 			eml.setScope("system");
 			eml.setXmllang("eng");
 			eml.setXmlns("eml");
-			
+
 			JAXBContext jaxbContext = JAXBContext.newInstance(Eml.class);
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
@@ -315,7 +409,7 @@ public class DwCAExporter
 			 * marshaling of java objects in xml (output to file and standard
 			 * output)
 			 */
-			jaxbMarshaller.marshal(eml, new File("eml.xml"));
+			jaxbMarshaller.marshal(eml, new File(FILE_NAME_EML));
 			jaxbMarshaller.marshal(eml, System.out);
 
 		} catch (JAXBException e)
--- nl.naturalis.nda.export/src/main/java/nl/naturalis/nda/export/dwca/Meta.java
+++ nl.naturalis.nda.export/src/main/java/nl/naturalis/nda/export/dwca/Meta.java
@@ -1,219 +1,340 @@
 package nl.naturalis.nda.export.dwca;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-import javax.xml.bind.annotation.XmlType;
 
+@XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "archive")
-@XmlType(propOrder = {"unitID", "unitGUID", "collectorsFieldNumber", "assemblageID",
-		              "sourceInstitutionID", "sourceID", "owner", "licenceType", "licence",
-		              "recordBasis", "kindOfUnit", "collectionType", "typeStatus","sex",
-		              "phaseOrStage","title","notes","preparationType","numberOfSpecimen","fromCaptivity","objectPublic","multiMediaPublic"})
-
 public class Meta
 {
+	@XmlAttribute(name = "metadata")
+	private String metadata;
+	@XmlAttribute(name = "xmlns:xsi")
+	private String xmlnsxsi;
+	@XmlAttribute(name = "xmlns:tdwg")
+	private String xmlnstdwg;
+	@XmlElement(name = "core")
+	List<Core> cores;
+
+	@XmlElement(name = "unitid")
 	private String unitID;
+	@XmlElement(name = "unitguid")
 	private String unitGUID;
+	@XmlElement(name = "collectorsfieldnumber")
 	private String collectorsFieldNumber;
+	@XmlElement(name = "assemblageid")
 	private String assemblageID;
+	@XmlElement(name = "sourceinstitutionid")
 	private String sourceInstitutionID;
+	@XmlElement(name = "sourceid")
 	private String sourceID;
+	@XmlElement(name = "owner")
 	private String owner;
+	@XmlElement(name = "licencetype")
 	private String licenceType;
+	@XmlElement(name = "licence")
 	private String licence;
+	@XmlElement(name = "recordbasis")
 	private String recordBasis;
+	@XmlElement(name = "kindofunit")
 	private String kindOfUnit;
+	@XmlElement(name = "collectiontype")
 	private String collectionType;
+	@XmlElement(name = "typestatus")
 	private String typeStatus;
+	@XmlElement(name = "sex")
 	private String sex;
+	@XmlElement(name = "phaseorstage")
 	private String phaseOrStage;
+	@XmlElement(name = "title")
 	private String title;
+	@XmlElement(name = "notes")
 	private String notes;
+	@XmlElement(name = "preparationtype")
 	private String preparationType;
+	@XmlElement(name = "numberofspecimen")
 	private int numberOfSpecimen;
+	@XmlElement(name = "fromcaptivity")
 	private boolean fromCaptivity;
+	@XmlElement(name = "objectpublic")
 	private boolean objectPublic;
+	@XmlElement(name = "multimediapublic")
 	private boolean multiMediaPublic;
-	
-	
+
 	public String getUnitID()
 	{
 		return unitID;
 	}
+
 	public void setUnitID(String unitID)
 	{
 		this.unitID = unitID;
 	}
+
 	public String getUnitGUID()
 	{
 		return unitGUID;
 	}
+
 	public void setUnitGUID(String unitGUID)
 	{
 		this.unitGUID = unitGUID;
 	}
+
 	public String getCollectorsFieldNumber()
 	{
 		return collectorsFieldNumber;
 	}
+
 	public void setCollectorsFieldNumber(String collectorsFieldNumber)
 	{
 		this.collectorsFieldNumber = collectorsFieldNumber;
 	}
+
 	public String getAssemblageID()
 	{
 		return assemblageID;
 	}
+
 	public void setAssemblageID(String assemblageID)
 	{
 		this.assemblageID = assemblageID;
 	}
+
 	public String getSourceInstitutionID()
 	{
 		return sourceInstitutionID;
 	}
+
 	public void setSourceInstitutionID(String sourceInstitutionID)
 	{
 		this.sourceInstitutionID = sourceInstitutionID;
 	}
+
 	public String getSourceID()
 	{
 		return sourceID;
 	}
+
 	public void setSourceID(String sourceID)
 	{
 		this.sourceID = sourceID;
 	}
+
 	public String getOwner()
 	{
 		return owner;
 	}
+
 	public void setOwner(String owner)
 	{
 		this.owner = owner;
 	}
+
 	public String getLicenceType()
 	{
 		return licenceType;
 	}
+
 	public void setLicenceType(String licenceType)
 	{
 		this.licenceType = licenceType;
 	}
+
 	public String getLicence()
 	{
 		return licence;
 	}
+
 	public void setLicence(String licence)
 	{
 		this.licence = licence;
 	}
+
 	public String getRecordBasis()
 	{
 		return recordBasis;
 	}
+
 	public void setRecordBasis(String recordBasis)
 	{
 		this.recordBasis = recordBasis;
 	}
+
 	public String getKindOfUnit()
 	{
 		return kindOfUnit;
 	}
+
 	public void setKindOfUnit(String kindOfUnit)
 	{
 		this.kindOfUnit = kindOfUnit;
 	}
+
 	public String getCollectionType()
 	{
 		return collectionType;
 	}
+
 	public void setCollectionType(String collectionType)
 	{
 		this.collectionType = collectionType;
 	}
+
 	public String getTypeStatus()
 	{
 		return typeStatus;
 	}
+
 	public void setTypeStatus(String typeStatus)
 	{
 		this.typeStatus = typeStatus;
 	}
+
 	public String getSex()
 	{
 		return sex;
 	}
+
 	public void setSex(String sex)
 	{
 		this.sex = sex;
 	}
+
 	public String getPhaseOrStage()
 	{
 		return phaseOrStage;
 	}
+
 	public void setPhaseOrStage(String phaseOrStage)
 	{
 		this.phaseOrStage = phaseOrStage;
 	}
+
 	public String getTitle()
 	{
 		return title;
 	}
+
 	public void setTitle(String title)
 	{
 		this.title = title;
 	}
+
 	public String getNotes()
 	{
 		return notes;
 	}
+
 	public void setNotes(String notes)
 	{
 		this.notes = notes;
 	}
+
 	public String getPreparationType()
 	{
 		return preparationType;
 	}
+
 	public void setPreparationType(String preparationType)
 	{
 		this.preparationType = preparationType;
 	}
+
 	public int getNumberOfSpecimen()
 	{
 		return numberOfSpecimen;
 	}
+
 	public void setNumberOfSpecimen(int numberOfSpecimen)
 	{
 		this.numberOfSpecimen = numberOfSpecimen;
 	}
+
 	public boolean isFromCaptivity()
 	{
 		return fromCaptivity;
 	}
+
 	public void setFromCaptivity(boolean fromCaptivity)
 	{
 		this.fromCaptivity = fromCaptivity;
 	}
+
 	public boolean isObjectPublic()
 	{
 		return objectPublic;
 	}
+
 	public void setObjectPublic(boolean objectPublic)
 	{
 		this.objectPublic = objectPublic;
 	}
+
 	public boolean isMultiMediaPublic()
 	{
 		return multiMediaPublic;
 	}
+
 	public void setMultiMediaPublic(boolean multiMediaPublic)
 	{
 		this.multiMediaPublic = multiMediaPublic;
+	}
+
+	public String getMetadata()
+	{
+		return metadata;
+	}
+
+	public void setMetadata(String metadata)
+	{
+		this.metadata = metadata;
+	}
+
+	public String getXmlnsxsi()
+	{
+		return xmlnsxsi;
+	}
+
+	public void setXmlnsxsi(String xmlnsxsi)
+	{
+		this.xmlnsxsi = xmlnsxsi;
+	}
+
+	public String getXmlnstdwg()
+	{
+		return xmlnstdwg;
+	}
+
+	public void setXmlnstdwg(String xmlnstdwg)
+	{
+		this.xmlnstdwg = xmlnstdwg;
+	}
+
+	public List<Core> getCores()
+	{
+		return cores;
+	}
+
+	public void setCores(List<Core> cores)
+	{
+		this.cores = cores;
+	}
+
+	public void add(Core cores)
+	{
+		if (this.cores == null)
+		{
+			this.cores = new ArrayList<Core>();
+		}
+		this.cores.add(cores);
+
 	}
 
 }

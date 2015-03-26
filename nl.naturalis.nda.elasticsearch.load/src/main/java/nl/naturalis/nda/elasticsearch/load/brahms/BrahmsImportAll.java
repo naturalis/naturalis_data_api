ae81234440269765374b691b088f6ac30307a671
rkartowikromo <Reinier.kartowikromo@naturalis.nl>
2015-03-26 15:04:33 +0100

    Aanpassing develop

--- nl.naturalis.nda.elasticsearch.load/src/main/java/nl/naturalis/nda/elasticsearch/load/brahms/BrahmsImportAll.java
+++ nl.naturalis.nda.elasticsearch.load/src/main/java/nl/naturalis/nda/elasticsearch/load/brahms/BrahmsImportAll.java
@@ -5,8 +5,6 @@ import static nl.naturalis.nda.elasticsearch.load.NDAIndexManager.LUCENE_TYPE_SP
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import nl.naturalis.nda.domain.SourceSystem;
 import nl.naturalis.nda.elasticsearch.client.IndexNative;
@@ -66,23 +64,23 @@ public class BrahmsImportAll {
 		logger.info("Ready");
 	}
 
-	static final Logger logger = LoggerFactory.getLogger(BrahmsImportAll.class);
+	private static final Logger logger = LoggerFactory.getLogger(BrahmsImportAll.class);
 
 	private final IndexNative index;
-	private final boolean rename;
+	private final boolean backup;
 
 
 	public BrahmsImportAll(IndexNative index)
 	{
 		this.index = index;
-		String prop = System.getProperty("rename", "false");
-		rename = prop.equals("1") || prop.equalsIgnoreCase("true");
+		String prop = System.getProperty("nl.naturalis.nda.elasticsearch.load.brahms.backup", "true");
+		backup = prop.equals("1") || prop.equalsIgnoreCase("true");
 	}
 
 
 	public void importCsvFiles() throws Exception
 	{
-		BrahmsExportEncodingConverter.convertFiles();
+		BrahmsDumpUtil.convertFiles();
 		String csvDir = LoadUtil.getConfig().required("brahms.csv_dir");
 		File file = new File(csvDir);
 		if (!file.isDirectory()) {
@@ -92,27 +90,23 @@ public class BrahmsImportAll {
 			@Override
 			public boolean accept(File dir, String name)
 			{
-				return name.toLowerCase().endsWith(".csv");
+				return name.toLowerCase().endsWith(BrahmsDumpUtil.FILE_EXT_PROCESSABLE);
 			}
 		});
 		if (files.length == 0) {
 			logger.info("No CSV files to process");
 			return;
 		}
-
 		int maxRecords = Integer.parseInt(System.getProperty("maxRecords", "0"));
-
 		BrahmsSpecimensImporter specimenImporter = new BrahmsSpecimensImporter(index);
 		specimenImporter.setMaxRecords(maxRecords);
 		BrahmsMultiMediaImporter mediaImporter = new BrahmsMultiMediaImporter(index);
 		mediaImporter.setMaxRecords(maxRecords);
-
 		for (File f : files) {
-			specimenImporter.importCsv(f.getCanonicalPath());
-			mediaImporter.importCsv(f.getCanonicalPath());
-			if (rename) {
-				String now = new SimpleDateFormat("yyyyMMdd").format(new Date());
-				f.renameTo(new File(f.getCanonicalPath() + "." + now + ".bak"));
+			specimenImporter.importCsv(f.getAbsolutePath());
+			mediaImporter.importCsv(f.getAbsolutePath());
+			if (backup) {
+				f.renameTo(new File(f.getCanonicalPath() + BrahmsDumpUtil.FILE_EXT_PROCESSED));
 			}
 		}
 	}

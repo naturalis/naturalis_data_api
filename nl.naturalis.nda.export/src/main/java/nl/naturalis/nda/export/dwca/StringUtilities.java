88b2f26cb23315d101c3b70064cf0b2c29e47110
Ayco Holleman <ayco.holleman@naturalis.nl>
2015-03-31 09:27:36 +0200

    NDA-291 changed config management

--- nl.naturalis.nda.export/src/main/java/nl/naturalis/nda/export/dwca/StringUtilities.java
+++ nl.naturalis.nda.export/src/main/java/nl/naturalis/nda/export/dwca/StringUtilities.java
@@ -15,12 +15,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+import nl.naturalis.nda.elasticsearch.load.LoadUtil;
+import nl.naturalis.nda.export.ExportException;
+
+import org.domainobject.util.ConfigObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,7 +35,9 @@ public class StringUtilities {
 	private static final String propertiesExtension = ".properties";
 	static String propertiesfilename = null;
 
-	public static int indexOfFirstContainedCharacter(String s1, String s2) {
+
+	public static int indexOfFirstContainedCharacter(String s1, String s2)
+	{
 		if (s1 == null || s1.isEmpty())
 			return -1;
 		Set<Character> set = new HashSet<Character>();
@@ -44,6 +51,7 @@ public class StringUtilities {
 		}
 		return -1; // No matches.
 	}
+
 
 	/*
 	 * Example: try { String zipfilename = zipFileName + zipExtension;
@@ -61,8 +69,8 @@ public class StringUtilities {
 	 * (IOException e) { e.printStackTrace(); }
 	 */
 	/* Zip multiple files */
-	public static void addToZipFile(String fileName, ZipOutputStream zos)
-			throws FileNotFoundException, IOException {
+	public static void addToZipFile(String fileName, ZipOutputStream zos) throws FileNotFoundException, IOException
+	{
 		System.out.println("Writing '" + fileName + "' to zip file");
 		logger.info("Writing '" + fileName + "' to zip file");
 
@@ -85,16 +93,17 @@ public class StringUtilities {
 		fis.close();
 	}
 
+
 	/* Read the value from properties file */
-	public static String getPropertyValue(String propertyname, String value) {
+	public static String getPropertyValue(String propertyname, String value)
+	{
 		propertiesfilename = propertyname + propertiesExtension;
 		String result = null;
 		boolean found = false;
 		Properties prop = new Properties();
 		try {
 			/* load a properties file */
-			prop.load(StringUtilities.class.getClassLoader()
-					.getResourceAsStream(propertiesfilename));
+			prop.load(StringUtilities.class.getClassLoader().getResourceAsStream(propertiesfilename));
 			Enumeration<?> e = prop.propertyNames();
 			while (e.hasMoreElements() && !found) {
 				String propertyName = (String) e.nextElement();
@@ -105,57 +114,46 @@ public class StringUtilities {
 					break;
 				}
 			}
-		} catch (IOException ex) {
+		}
+		catch (IOException ex) {
 			ex.printStackTrace();
 		}
 		return result;
 	}
 
+
 	/* Read the value from properties file */
-	public static String readPropertyvalue(String propertyname, String key) {
+	public static String readPropertyvalue(String propertyname, String key)
+	{
 		propertiesfilename = propertyname + propertiesExtension;
 		String result = null;
 		Properties prop = new Properties();
 		try {
 			/* load a properties file */
 			if (propertiesfilename != null) {
-				prop.load(StringUtilities.class.getClassLoader()
-						.getResourceAsStream(propertiesfilename));
+				prop.load(StringUtilities.class.getClassLoader().getResourceAsStream(propertiesfilename));
 				result = prop.getProperty(key);
 			}
 
-		} catch (IOException ex) {
+		}
+		catch (IOException ex) {
 			ex.printStackTrace();
 		}
 		return result;
 	}
 
-	/* Create Output directory */
-	public static void createOutPutDirectory() {
-		File directory = new File(readPropertyvalue("OutPut", "Directory"));
-		boolean result = false;
-		if (!directory.exists()) {
-			try {
-				directory.mkdir();
-				result = true;
-			} catch (SecurityException se) {
-				se.printStackTrace();
-			}
-			if (result) {
-				logger.info("DwCAExport directory was created successfull.");
-			}
-		}
-	}
 
 	/* Create Output Zip directory. */
-	public static void createZipOutPutDirectory() {
+	public static void createZipOutPutDirectory()
+	{
 		File directory = new File(readPropertyvalue("OutPut", "ZipDirectory"));
 		boolean result = false;
 		if (!directory.exists()) {
 			try {
 				directory.mkdir();
 				result = true;
-			} catch (SecurityException se) {
+			}
+			catch (SecurityException se) {
 				se.printStackTrace();
 			}
 			if (result) {
@@ -164,8 +162,9 @@ public class StringUtilities {
 		}
 	}
 
+
 	/* Create Archive Zip directory. */
-	public static void createArchiveZipDirectory() 
+	public static void createArchiveZipDirectory()
 	{
 		File directory = new File(readPropertyvalue("OutPut", "ZipArchiveDirectory"));
 		boolean result = false;
@@ -173,7 +172,8 @@ public class StringUtilities {
 			try {
 				directory.mkdir();
 				result = true;
-			} catch (SecurityException se) {
+			}
+			catch (SecurityException se) {
 				se.printStackTrace();
 			}
 			if (result) {
@@ -182,20 +182,23 @@ public class StringUtilities {
 		}
 	}
 
+
 	/*
 	 * if value is '1' field will be added to CSV file otherwise if value is
 	 * '0'. field will not be added.
 	 */
-	public static boolean isFieldChecked(String propertyname, String value) {
+	public static boolean isFieldChecked(String propertyname, String value)
+	{
 		int commaindex = StringUtilities.getPropertyValue(propertyname, value).length() - 1;
 		String result = StringUtilities.getPropertyValue(propertyname, value);
 		String resultprop = result.substring(commaindex);
 		return resultprop.matches("1(.*)");
 	}
 
+
 	/* Copy a file from a Source directory to a Destination directory */
-	public static void CopyAFile(File sourceFile, File DestinationFile)
-			throws IOException {
+	public static void CopyAFile(File sourceFile, File DestinationFile) throws IOException
+	{
 		InputStream inputstream = null;
 		OutputStream outputstream = null;
 		try {
@@ -206,24 +209,22 @@ public class StringUtilities {
 			while ((length = inputstream.read(buffer)) > 0) {
 				outputstream.write(buffer, 0, length);
 			}
-		} finally 
-		{
-			if (inputstream != null) 
-			{
+		}
+		finally {
+			if (inputstream != null) {
 				inputstream.close();
 			}
-			if (outputstream != null) 
-			{
+			if (outputstream != null) {
 				outputstream.close();
 			}
 		}
 	}
 
+
 	/* Renamed the zipfile extension ".zip" file to ".zip.bak" */
-	public static void renameDwCAZipFile(File fileToRenamed) 
+	public static void renameDwCAZipFile(File fileToRenamed)
 	{
-		if (fileToRenamed.exists()) 
-		{
+		if (fileToRenamed.exists()) {
 			int index = fileToRenamed.getName().indexOf(".");
 			String filename = fileToRenamed.getName().substring(0, index);
 			File path = fileToRenamed.getParentFile();
@@ -234,50 +235,205 @@ public class StringUtilities {
 				logger.info("File '" + filebak + "' successfull deleted.");
 			}
 			boolean success = fileToRenamed.renameTo(new File(bakpath));
-			if (success) 
-			{
+			if (success) {
 				logger.info("File successfull renamed to '" + bakpath + "'");
-			} else 
-			{
-				logger.info("File in '" + fileToRenamed
-						+ "' not successfull renamed.");
+			}
+			else {
+				logger.info("File in '" + fileToRenamed + "' not successfull renamed.");
 			}
 		}
 	}
 
+
 	/* Renamed the predifined eml.xml file from source directory to eml.xml */
-	public static void renameDwCAEMLFile(File emlFileToRenamed) 
+	public static void renameDwCAEMLFile(File emlFileToRenamed)
 	{
-		if (emlFileToRenamed.exists()) 
-		{
+		if (emlFileToRenamed.exists()) {
 			int index = emlFileToRenamed.getName().indexOf(".");
 			String filename = emlFileToRenamed.getName().substring(index - 3);
 			File path = emlFileToRenamed.getParentFile();
-			String emlpath = path + "\\" + filename; 
+			String emlpath = path + "\\" + filename;
 			File emlfile = new File(emlpath);
-			if (emlfile.exists()) 
-			{
+			if (emlfile.exists()) {
 				emlfile.delete();
 				logger.info("File '" + emlfile + "' successfull deleted.");
 			}
 			boolean success = emlFileToRenamed.renameTo(new File(emlpath));
 			if (success) {
 				logger.info("File successfull renamed to '" + emlpath + "'");
-			} else {
-				logger.info("File in '" + emlpath
-						+ "' not successfull renamed.");
+			}
+			else {
+				logger.info("File in '" + emlpath + "' not successfull renamed.");
 			}
 		}
 	}
 
-	public static boolean stringContainsItemFromList(String inputString,
-			String[] items) {
+
+	public static boolean stringContainsItemFromList(String inputString, String[] items)
+	{
 		for (int i = 0; i < items.length; i++) {
 			if (inputString.contains(items[i])) {
 				return true;
 			}
 		}
 		return false;
+	}
+
+
+	public static File newFile(File directory, String fileName)
+	{
+		String sep = System.getProperty("file.separator");
+		return new File(directory.getAbsolutePath() + sep + fileName);
+	}
+
+
+	public static String getFullPath(File directory, String fileName)
+	{
+		String sep = System.getProperty("file.separator");
+		return directory.getAbsolutePath() + sep + fileName;
+	}
+
+
+	/**
+	 * Root directory for output from the DwCA export program
+	 * 
+	 * @return
+	 */
+	public static File getDwcaExportDir()
+	{
+		String path = LoadUtil.getConfig().required("dwca.export.dir");
+		File rootDir = new File(path);
+		if (!rootDir.isDirectory() || !rootDir.canWrite()) {
+			throw new ExportException(String.format("Directory does not exist or is not writable: \"%s\"", path));
+		}
+		return rootDir;
+	}
+
+
+	/**
+	 * Working directory for the DwCA export program: ${dwcaExportDir}/output
+	 * 
+	 * @return
+	 */
+	public static File getWorkingDirectory()
+	{
+		File outputDir = newFile(getDwcaExportDir(), "output");
+		if (!outputDir.isDirectory()) {
+			outputDir.mkdir();
+		}
+		return outputDir;
+	}
+
+
+	/**
+	 * Directory into which the DwC archive files will be written:
+	 * ${dwcaExportDir}/zip
+	 * 
+	 * @return
+	 */
+	public static File getZipOutputDirectory()
+	{
+		File zipDir = newFile(getDwcaExportDir(), "zip");
+		if (!zipDir.isDirectory()) {
+			zipDir.mkdir();
+		}
+		return zipDir;
+	}
+
+
+	/**
+	 * Backup directory for the DwC archive files: ${dwcaExportDir}/bak
+	 * 
+	 * @return
+	 */
+	public static File getBackupDirectory()
+	{
+		File bakDir = newFile(getDwcaExportDir(), "bak");
+		if (!bakDir.isDirectory()) {
+			bakDir.mkdir();
+		}
+		return bakDir;
+	}
+
+
+	/**
+	 * Root directory for input for the DwCA export program
+	 * 
+	 * @return
+	 */
+	public static File getDwcaConfigDir()
+	{
+		String path = LoadUtil.getConfig().required("dwca.config.dir");
+		File confDir = new File(path);
+		if (!confDir.isDirectory()) {
+			throw new ExportException(String.format("No such directory: \"%s\"", path));
+		}
+		return confDir;
+	}
+
+
+	/**
+	 * Directory containing EML files: ${dwcaConfigDir}/eml
+	 * 
+	 * @return
+	 */
+	public static File getEmlDirectory()
+	{
+		File emlDir = newFile(getDwcaConfigDir(), "eml");
+		if (!emlDir.isDirectory()) {
+			throw new ExportException(String.format("No such directory: \"%s\"", emlDir.getAbsolutePath()));
+		}
+		return emlDir;
+	}
+
+
+	/**
+	 * Directory containing collection-related property files:
+	 * ${dwcaConfigDir}/config
+	 * 
+	 * @return
+	 */
+	public static File getPropertiesDir()
+	{
+		File propsDir = newFile(getDwcaConfigDir(), "config");
+		if (!propsDir.isDirectory()) {
+			throw new ExportException(String.format("No such directory: \"%s\"", propsDir.getAbsolutePath()));
+		}
+		return propsDir;
+	}
+
+	// Cache for collection configuration settings
+	private static final HashMap<String, ConfigObject> collectionProps = new HashMap<String, ConfigObject>();
+
+
+	/**
+	 * Get all configuration settings for a collection
+	 * 
+	 * @param collectionName
+	 * @return
+	 */
+	public static ConfigObject getProperties(String collectionName)
+	{
+		ConfigObject cfg = collectionProps.get(collectionName);
+		if (cfg == null) {
+			File propsFile = newFile(getPropertiesDir(), collectionName + ".properties");
+			cfg = new ConfigObject(propsFile);
+			collectionProps.put(collectionName, cfg);
+		}
+		return cfg;
+	}
+
+
+	/**
+	 * Get a configuration setting for a collection
+	 * 
+	 * @param collectionName
+	 * @param propertyName
+	 * @return
+	 */
+	public static String getProperty(String collectionName, String propertyName)
+	{
+		return getProperties(collectionName).required(propertyName);
 	}
 
 }

e296c84bb146bd001c6cb474d3aa40956d5bb117
Ayco Holleman <ayco.holleman@naturalis.nl>
2015-05-01 10:15:01 +0200

    Added docs directory

---
+++ nl.naturalis.nda.service.rest/src/main/java/nl/naturalis/nda/service/rest/NbaApplication.java
@@ -0,0 +1,17 @@
+package nl.naturalis.nda.service.rest;
+
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/*@ApplicationPath("/")*/
+public class NbaApplication /*extends Application*/ {
+
+	//@Override
+	public Set<Class<?>> getClasses()
+	{
+		return null;//super.getClasses();
+	}	
+
+}

daadcbf564889853afc9249db7ec4bb7bc62a865
Ayco Holleman <ayco.holleman@naturalis.nl>
2015-05-04 15:32:48 +0200

    fixed bug in brahms import shell script

--- nl.naturalis.nda.sh/import/sh/import-brahms.sh
+++ nl.naturalis.nda.sh/import/sh/import-brahms.sh
@@ -2,24 +2,27 @@
 
 . ./include.sh
 
-# Rename export files after they have been processed?
-rename='false'
+# Rename dump files after they have been processed?
+# (Only applicable when importing both specimens and
+# multimedia. Otherwise dump files are never renamed)
+rename=true
+
 # Number of ES index requests bundled together
 bulkRequestSize=1000
 
-JAVA_OPTS="$JAVA_OPTS -DbulkRequestSize=${bulkRequestSize}"
-JAVA_OPTS="$JAVA_OPTS -Drename=${rename}"
+JAVA_OPTS="$JAVA_OPTS -Dnl.naturalis.nda.elasticsearch.load.brahms.batchsize=${bulkRequestSize}"
+JAVA_OPTS="$JAVA_OPTS -Dnl.naturalis.nda.elasticsearch.load.brahms.backup=${rename}"
 
 type="${1}"
 
 if [ "$type" = specimens ]
 then
     java -cp ${classpath} $JAVA_OPTS ${loadPackage}.brahms.BrahmsSpecimensImporter
-	echo "WARNING: When not loading both specimens and multimedia file backup is disabled"
+	echo "WARNING: When not loading only specimens or only multimedia file backup is disabled"
 elif [ "$type" = multimedia ]
 then
     java -cp ${classpath} $JAVA_OPTS ${loadPackage}.brahms.BrahmsMultiMediaImporter
-	echo "WARNING: When not loading both specimens and multimedia file backup is disabled"
+	echo "WARNING: When not loading only specimens or only multimedia file backup is disabled"
 elif [ "$type" = "" ]
 then
     java -cp ${classpath} $JAVA_OPTS ${loadPackage}.brahms.BrahmsImportAll

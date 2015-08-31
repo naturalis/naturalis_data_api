0c55e1cf5e1a01007d65056b799103eda145dc0c
Ayco Holleman <ayco.holleman@naturalis.nl>
2015-08-31 12:31:04 +0200

    NDA-469 added deserializer for GatheringSiteCoordinates

---
+++ nl.naturalis.nda.client/src/main/java/nl/naturalis/nda/client/GatheringSiteCoordinatesDeserializer.java
@@ -0,0 +1,27 @@
+package nl.naturalis.nda.client;
+
+import java.io.IOException;
+
+import nl.naturalis.nda.domain.GatheringSiteCoordinates;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+
+class GatheringSiteCoordinatesDeserializer extends JsonDeserializer<GatheringSiteCoordinates> {
+
+	@Override
+	public GatheringSiteCoordinates deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException
+	{
+		JsonNode node = jp.getCodec().readTree(jp);
+		JsonNode n = node.get("latitudeDecimal");
+		Double lat = n.isNull()? null : Double.valueOf(n.textValue());
+		n = node.get("longitudeDecimal");
+		Double lon = n.isNull()? null : Double.valueOf(n.textValue());
+		return new GatheringSiteCoordinates(lat, lon);
+	}
+
+
+}

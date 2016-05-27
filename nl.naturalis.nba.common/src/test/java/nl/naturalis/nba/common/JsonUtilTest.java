03580e677a203a58ad25e5ce9d1c0966c74d30a7
Ayco Holleman <ayco.holleman@naturalis.nl>
2016-05-27 12:16:20 +0200

    Added git dummy files to empty folders and changed syntax error in es-settings.json

---
+++ nl.naturalis.nba.common/src/test/java/nl/naturalis/nba/common/JsonUtilTest.java
@@ -0,0 +1,45 @@
+package nl.naturalis.nba.common;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class JsonUtilTest {
+
+	@Test
+	public void testSerialize()
+	{
+	}
+
+	@Test
+	public void testDeserialize()
+	{
+	}
+
+	@Test
+	public void testToJson()
+	{
+	}
+
+	@Test
+	public void testToPrettyJsonObject()
+	{
+	}
+
+	@Test
+	public void testToPrettyJsonObjectBoolean()
+	{
+	}
+
+	@Test
+	public void testFromJson()
+	{
+	}
+
+	@Test
+	public void testConvert()
+	{
+	}
+
+}

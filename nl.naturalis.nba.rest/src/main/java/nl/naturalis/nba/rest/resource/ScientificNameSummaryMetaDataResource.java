0dc898bb285fe5c33860a415292a2c6507b1ef6f
Ayco Holleman <ayco.holleman@naturalis.nl>
2017-01-25 16:02:02 +0100

    Added metadata dao/resource for ScientificName summary

---
+++ nl.naturalis.nba.rest/src/main/java/nl/naturalis/nba/rest/resource/ScientificNameSummaryMetaDataResource.java
@@ -0,0 +1,81 @@
+package nl.naturalis.nba.rest.resource;
+
+import static nl.naturalis.nba.rest.util.ResourceUtil.JSON_CONTENT_TYPE;
+import static nl.naturalis.nba.rest.util.ResourceUtil.handleError;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import nl.naturalis.nba.api.ComparisonOperator;
+import nl.naturalis.nba.dao.ScientificNameSummaryMetaDataDao;
+import nl.naturalis.nba.utils.ConfigObject;
+
+@SuppressWarnings("static-method")
+@Path("/taxon/metadata")
+@Stateless
+@LocalBean
+public class ScientificNameSummaryMetaDataResource {
+
+	@SuppressWarnings("unused")
+	private static final Logger logger = LogManager
+			.getLogger(ScientificNameSummaryMetaDataResource.class);
+
+	@EJB
+	Registry registry;
+
+	@GET
+	@Path("/getMapping")
+	@Produces(JSON_CONTENT_TYPE)
+	public String getMapping(@Context UriInfo uriInfo)
+	{
+		try {
+			ScientificNameSummaryMetaDataDao dao = new ScientificNameSummaryMetaDataDao();
+			return dao.getMapping();
+		}
+		catch (Throwable t) {
+			throw handleError(uriInfo, t);
+		}
+	}
+
+	@GET
+	@Path("/getPaths")
+	@Produces(JSON_CONTENT_TYPE)
+	public String[] getPaths(@Context UriInfo uriInfo)
+	{
+		try {
+			ScientificNameSummaryMetaDataDao dao = new ScientificNameSummaryMetaDataDao();
+			String s = uriInfo.getQueryParameters().getFirst("sorted");
+			boolean sorted = ConfigObject.isTrueValue(s);
+			return dao.getPaths(sorted);
+		}
+		catch (Throwable t) {
+			throw handleError(uriInfo, t);
+		}
+	}
+
+	@GET
+	@Path("/isOperatorAllowed/{field}/{operator}")
+	@Produces(JSON_CONTENT_TYPE)
+	public boolean isOperatorAllowed(@PathParam("field") String field,
+			@PathParam("operator") String operator, @Context UriInfo uriInfo)
+	{
+		try {
+			ComparisonOperator op = ComparisonOperator.parse(operator);
+			ScientificNameSummaryMetaDataDao dao = new ScientificNameSummaryMetaDataDao();
+			return dao.isOperatorAllowed(field, op);
+		}
+		catch (Throwable t) {
+			throw handleError(uriInfo, t);
+		}
+	}
+}

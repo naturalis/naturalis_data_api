00ed509d9839513e7d6914c3fe3e0d287beca1ea
Ayco Holleman <ayco.holleman@naturalis.nl>
2017-03-14 11:13:25 +0100

    ok

--- nl.naturalis.nba.api/src/main/java/nl/naturalis/nba/api/model/Specimen.java
+++ nl.naturalis.nba.api/src/main/java/nl/naturalis/nba/api/model/Specimen.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import nl.naturalis.nba.api.annotations.Analyzers;
-import nl.naturalis.nba.api.annotations.NotNested;
 
 public class Specimen extends NbaTraceableObject implements IDocumentObject {
 
@@ -45,7 +44,6 @@ public class Specimen extends NbaTraceableObject implements IDocumentObject {
 	private List<SpecimenIdentification> identifications;
 	private List<String> theme;
 
-	@NotNested
 	private List<TaxonomicEnrichment> taxonomicEnrichments;
 
 	public void addIndentification(SpecimenIdentification identification)

b014d47a786bad0e2a960953c6a534454e4c2b56
Ayco Holleman <ayco.holleman@naturalis.nl>
2017-04-05 12:33:30 +0200

    added getRestServices API

--- nl.naturalis.nba.rest/src/main/java/nl/naturalis/nba/rest/resource/NbaMetaDataResource.java
+++ nl.naturalis.nba.rest/src/main/java/nl/naturalis/nba/rest/resource/NbaMetaDataResource.java
@@ -3,12 +3,23 @@ package nl.naturalis.nba.rest.resource;
 import static nl.naturalis.nba.rest.util.ResourceUtil.JSON_CONTENT_TYPE;
 import static nl.naturalis.nba.rest.util.ResourceUtil.handleError;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -16,6 +27,10 @@ import javax.ws.rs.core.UriInfo;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
 import nl.naturalis.nba.api.model.PhaseOrStage;
 import nl.naturalis.nba.api.model.Sex;
@@ -23,7 +38,9 @@ import nl.naturalis.nba.api.model.SourceSystem;
 import nl.naturalis.nba.api.model.SpecimenTypeStatus;
 import nl.naturalis.nba.api.model.TaxonomicStatus;
 import nl.naturalis.nba.api.model.metadata.NbaSetting;
+import nl.naturalis.nba.api.model.metadata.RestService;
 import nl.naturalis.nba.dao.NbaMetaDataDao;
+import nl.naturalis.nba.utils.StringUtil;
 
 @Path("/metadata")
 @Stateless
@@ -53,41 +70,126 @@ public class NbaMetaDataResource {
 	@GET
 	@Path("/getSourceSystems")
 	@Produces(JSON_CONTENT_TYPE)
-	public SourceSystem[] getSourceSystems()
+	public SourceSystem[] getSourceSystems(@Context UriInfo uriInfo)
 	{
-		return new NbaMetaDataDao().getSourceSystems();
+		try {
+			return new NbaMetaDataDao().getSourceSystems();
+		}
+		catch (Throwable t) {
+			throw handleError(uriInfo, t);
+		}
 	}
 
 	@GET
 	@Path("/getContolledList/Sex")
 	@Produces(JSON_CONTENT_TYPE)
-	public Sex[] getControlledListSex()
+	public Sex[] getControlledListSex(@Context UriInfo uriInfo)
 	{
-		return new NbaMetaDataDao().getControlledListSex();
+		try {
+			return new NbaMetaDataDao().getControlledListSex();
+		}
+		catch (Throwable t) {
+			throw handleError(uriInfo, t);
+		}
 	}
 
 	@GET
 	@Path("/getContolledList/PhaseOrStage")
 	@Produces(JSON_CONTENT_TYPE)
-	public PhaseOrStage[] getControlledListPhaseOrStage()
+	public PhaseOrStage[] getControlledListPhaseOrStage(@Context UriInfo uriInfo)
 	{
-		return new NbaMetaDataDao().getControlledListPhaseOrStage();
+		try {
+			return new NbaMetaDataDao().getControlledListPhaseOrStage();
+		}
+		catch (Throwable t) {
+			throw handleError(uriInfo, t);
+		}
 	}
 
 	@GET
 	@Path("/getContolledList/TaxonomicStatus")
 	@Produces(JSON_CONTENT_TYPE)
-	public TaxonomicStatus[] getControlledListTaxonomicStatus()
+	public TaxonomicStatus[] getControlledListTaxonomicStatus(@Context UriInfo uriInfo)
 	{
-		return new NbaMetaDataDao().getControlledListTaxonomicStatus();
+		try {
+			return new NbaMetaDataDao().getControlledListTaxonomicStatus();
+		}
+		catch (Throwable t) {
+			throw handleError(uriInfo, t);
+		}
 	}
 
 	@GET
 	@Path("/getContolledList/SpecimenTypeStatus")
 	@Produces(JSON_CONTENT_TYPE)
-	public SpecimenTypeStatus[] getControlledListSpecimenTypeStatus()
+	public SpecimenTypeStatus[] getControlledListSpecimenTypeStatus(@Context UriInfo uriInfo)
 	{
-		return new NbaMetaDataDao().getControlledListSpecimenTypeStatus();
+		try {
+			return new NbaMetaDataDao().getControlledListSpecimenTypeStatus();
+		}
+		catch (Throwable t) {
+			throw handleError(uriInfo, t);
+		}
+	}
+
+	private static RestService[] restServices;
+
+	@GET
+	@Path("/getRestServices")
+	@Produces(JSON_CONTENT_TYPE)
+	public RestService[] getRestServices(@Context UriInfo uriInfo)
+	{
+		if (restServices == null) {
+
+			List<Class<? extends Annotation>> httpMethodAnnotations;
+			httpMethodAnnotations = Arrays.asList(DELETE.class, GET.class, POST.class, PUT.class);
+			
+			String baseUrl = StringUtil.rtrim(uriInfo.getBaseUri().toString(), '/');
+
+			ConfigurationBuilder config = new ConfigurationBuilder();
+			config.setUrls(ClasspathHelper.forPackage(getClass().getPackage().getName()));
+			config.setScanners(new MethodAnnotationsScanner());
+			Reflections reflections = new Reflections(config);
+			Set<Method> resourceMethods = reflections.getMethodsAnnotatedWith(Path.class);
+			ArrayList<RestService> services = new ArrayList<>(100);
+			for (Method method : resourceMethods) {
+				RestService service = new RestService();
+				services.add(service);
+				Class<?> resourceClass = method.getDeclaringClass();
+				String rootPath = resourceClass.getDeclaredAnnotation(Path.class).value();
+				String path = method.getDeclaredAnnotation(Path.class).value();
+				String endPoint = rootPath + path;
+				endPoint=endPoint.replace("//", "/");
+				service.setEndPoint(endPoint);
+				service.setUrl(baseUrl + endPoint);
+				for (Class<? extends Annotation> c : httpMethodAnnotations) {
+					if (method.getDeclaredAnnotation(c) != null) {
+						service.setMethod(c.getSimpleName());
+						break;
+					}
+				}
+				if (method.getDeclaredAnnotation(Produces.class) != null) {
+					service.setProduces(method.getDeclaredAnnotation(Produces.class).value()[0]);
+				}
+				if (method.getDeclaredAnnotation(Consumes.class) != null) {
+					service.setConsumes(method.getDeclaredAnnotation(Consumes.class).value()[0]);
+				}
+			}
+			restServices = services.toArray(new RestService[services.size()]);
+			Arrays.sort(restServices, new Comparator<RestService>() {
+
+				@Override
+				public int compare(RestService o1, RestService o2)
+				{
+					int i = o1.getEndPoint().compareTo(o2.getEndPoint());
+					if (i == 0) {
+						i = o1.getMethod().compareTo(o2.getMethod());
+					}
+					return i;
+				}
+			});
+		}
+		return restServices;
 	}
 
 }

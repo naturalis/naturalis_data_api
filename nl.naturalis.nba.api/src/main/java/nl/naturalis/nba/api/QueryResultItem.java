8f33f7a12c062ad8f5327d972df8f2ca5b3b3cd4
Ayco Holleman <ayco.holleman@naturalis.nl>
2017-04-11 15:17:36 +0200

    NBAVTWO-325

--- nl.naturalis.nba.api/src/main/java/nl/naturalis/nba/api/QueryResultItem.java
+++ nl.naturalis.nba.api/src/main/java/nl/naturalis/nba/api/QueryResultItem.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import nl.naturalis.nba.api.model.IDocumentObject;
+
 /**
  * A {@code QueryResultItem} contains a document (or other type of object)
  * returned from a {@link QuerySpec query}. Besides the document itself a
@@ -13,7 +15,9 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
  * @author Ayco Holleman
  *
  * @param <T>
- *            The type of object coming back from the query.
+ *            The type of object coming back from the query. Note that while
+ *            this will often be an instance of {@link IDocumentObject}, this is
+ *            not required by the {@code QueryResultItem} class.
  */
 @JsonPropertyOrder({ "score", "item" })
 public class QueryResultItem<T> {

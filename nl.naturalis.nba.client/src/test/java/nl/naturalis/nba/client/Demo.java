4ac2f9923e4a71b34943d598f8092a14ffd01067
tomgilissen <tom.gilissen@naturalis.nl>
2017-04-28 16:31:25 +0200

    Enkele tests in Demo

--- nl.naturalis.nba.client/src/test/java/nl/naturalis/nba/client/Demo.java
+++ nl.naturalis.nba.client/src/test/java/nl/naturalis/nba/client/Demo.java
@@ -1,20 +1,41 @@
 package nl.naturalis.nba.client;
 
-import nl.naturalis.nba.api.IScientificNameGroupAccess;
-import nl.naturalis.nba.api.InvalidQueryException;
-import nl.naturalis.nba.api.QueryCondition;
-import nl.naturalis.nba.api.QueryResult;
-import nl.naturalis.nba.api.QuerySpec;
-import nl.naturalis.nba.api.ScientificNameGroupQuerySpec;
+import nl.naturalis.nba.api.model.Specimen;
 
 public class Demo {
 
-	public static void main(String[] args) throws InvalidQueryException
+	public static void main(String[] args)
 	{
+
+		// String baseUrl = "http://localhost:8080/v2";
+		String baseUrl = "http://145.136.242.164:8080/v2";
+		
+		ClientConfig config = new ClientConfig();
+		config.setBaseUrl(baseUrl);
+		
+		NbaSession session = new NbaSession(config);
+		
+		SpecimenClient specimenClient = session.getSpecimenClient(); 
+		Specimen specimenResults[] = specimenClient.findByUnitID("WAG.1706236");
+		for (Specimen specimen : specimenResults) 
+		{
+			ClientUtil.printTerse(specimen);
+		}
+		
+		System.out.println("\n");
+		
+		// http://145.136.242.164:8080/v2/geo/getGeoJsonForLocality/Leiden
+		
+		
+		GeoAreaClient geoClient = session.getGeoAreaClient();
+		System.out.println(geoClient.getGeoJsonForLocality("Leiden").getClass());
+		
+
+		
 		// Start an NBA session
-		String baseUrl = "http://localhost:8080/v2";
+//		String baseUrl = "http://localhost:8080/v2";
 		//String baseUrl = "http://145.136.242.164:8080/v2";
-		NbaSession session = new NbaSession(new ClientConfig(baseUrl));
+//		NbaSession session = new NbaSession(new ClientConfig(baseUrl));
 
 		//		ISpecimenAccess client = session.getSpecimenClient();		
 		//		String field = "identifications.taxonomicEnrichments.sourceSystem.code";
@@ -31,15 +52,15 @@ public class Demo {
 		//		QueryResult<?> result = client.query(query);
 		//		ClientUtil.printTerse(result);
 
-		IScientificNameGroupAccess client = session.getNameGroupClient();
-		QueryCondition condition0 = new QueryCondition("specimenCount", ">", 10);
-		QueryCondition condition1 = new QueryCondition("specimens.gatheringEvent.gatheringPersons.fullName", "=", "Springer, LA");
-		
-		ScientificNameGroupQuerySpec query = new ScientificNameGroupQuerySpec();
-		query.setConstantScore(true);
-		query.addCondition(condition0);
+//		IScientificNameGroupAccess client = session.getNameGroupClient();
+//		QueryCondition condition0 = new QueryCondition("specimenCount", ">", 10);
+//		QueryCondition condition1 = new QueryCondition("specimens.gatheringEvent.gatheringPersons.fullName", "=", "Springer, LA");
+//		
+//		ScientificNameGroupQuerySpec query = new ScientificNameGroupQuerySpec();
+//		query.setConstantScore(true);
+//		query.addCondition(condition0);
 		//query.addCondition(condition1);
-		QueryResult<?> result = client.querySpecial(query);
+//		QueryResult<?> result = client.querySpecial(query);
 		//ClientUtil.printTerse(result);
 
 		//		ITaxonAccess client = session.getTaxonClient();
@@ -47,6 +68,8 @@ public class Demo {
 		//		QueryCondition condition = new QueryCondition(field, "!=", null);
 		//		QuerySpec query = new QuerySpec();
 		//		query.addCondition(condition);
+		
+		
 
 	}
 

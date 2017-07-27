1deb8c1d13b9bef6148de01f186e6d71a12cf155
Ayco Holleman <ayco.holleman@naturalis.nl>
2017-07-27 07:46:43 +0200

    unit tests

--- nl.naturalis.nba.dao/src/test/java/nl/naturalis/nba/dao/format/DocumentFlattenerTest.java
+++ nl.naturalis.nba.dao/src/test/java/nl/naturalis/nba/dao/format/DocumentFlattenerTest.java
@@ -4,66 +4,66 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import nl.naturalis.nba.api.Path;
-import nl.naturalis.nba.dao.format.DocumentFlattener;
-import nl.naturalis.nba.dao.format.EntityObject;
+import nl.naturalis.nba.api.model.Person;
+import nl.naturalis.nba.api.model.Reference;
+import nl.naturalis.nba.api.model.ScientificName;
+import nl.naturalis.nba.api.model.SourceSystem;
+import nl.naturalis.nba.api.model.Taxon;
+import nl.naturalis.nba.api.model.VernacularName;
 
 public class DocumentFlattenerTest {
 
-	private Map<String, Object> taxon;
-	private Map<String, Object> sourceSystem;
-	private Map<String, Object> acceptedName;
-	private Map<String, Object> vernacularName0;
-	private Map<String, Object> vernacularName1;
-	private Map<String, Object> reference0;
-	private Map<String, Object> reference1;
-	private List<Map<String, Object>> references;
-	private List<Map<String, Object>> vernacularNames;
+	private Taxon taxon;
+	private SourceSystem sourceSystem;
+	private ScientificName acceptedName;
+	private VernacularName vernacularName0;
+	private VernacularName vernacularName1;
+	private Reference reference0;
+	private Reference reference1;
+	private List<Reference> references;
+	private List<VernacularName> vernacularNames;
 
 	@Before
 	public void init()
 	{
-		taxon = new HashMap<>();
-		taxon.put("sourceSystemId", "1");
+		taxon = new Taxon();
+		taxon.setSourceSystemId("1");
 
-		sourceSystem = new HashMap<>();
-		sourceSystem.put("code", "NSR");
-		sourceSystem.put("name", "Nederlands Soortenregister");
-		taxon.put("sourceSystem", sourceSystem);
+		sourceSystem = SourceSystem.NSR;
+		taxon.setSourceSystem(sourceSystem);
 
-		acceptedName = new HashMap<>();
-		acceptedName.put("fullScientificName", "Larus fuscus");
-		taxon.put("acceptedName", acceptedName);
+		acceptedName = new ScientificName();
+		acceptedName.setFullScientificName("Larus fuscus");
+		taxon.setAcceptedName(acceptedName);
 
-		vernacularName0 = new HashMap<>();
-		vernacularName0.put("name", "meeuw");
-		vernacularName0.put("language", "nl");
+		vernacularName0 = new VernacularName();
+		vernacularName0.setName("meeuw");
+		vernacularName0.setLanguage("nl");
 
-		vernacularName1 = new HashMap<>();
-		vernacularName1.put("name", "sea gull");
-		vernacularName1.put("language", "en");
+		vernacularName1 = new VernacularName();
+		vernacularName1.setName("sea gull");
+		vernacularName1.setLanguage("en");
 
-		reference0 = new HashMap<>();
-		reference0.put("title", "Vogels van Europa");
-		reference0.put("author", "Ruud Altenburg");
+		reference0 = new Reference();
+		reference0.setTitleCitation("Vogels van Europa");
+		reference0.setAuthor(new Person("Ruud Altenburg"));
 
-		reference1 = new HashMap<>();
-		reference1.put("title", "The Secret Life of Sea Gulls");
-		reference1.put("author", "John Smith");
+		reference1 = new Reference();
+		reference1.setTitleCitation("The Secret Life of Sea Gulls");
+		reference1.setAuthor(new Person("John Smith"));
 
 		references = Arrays.asList(reference0, reference1);
-		vernacularName0.put("references", references);
-		vernacularName1.put("references", references);
+		vernacularName0.setReferences(references);
+		vernacularName1.setReferences(references);
 
 		vernacularNames = Arrays.asList(vernacularName0, vernacularName1);
-		taxon.put("vernacularNames", vernacularNames);
+		taxon.setVernacularNames(vernacularNames);
 
 	}
 

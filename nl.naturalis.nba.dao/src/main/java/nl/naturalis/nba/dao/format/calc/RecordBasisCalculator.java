dff2e73d0caa44ebf543332cb6afcf42d8b5c48a
Ayco Holleman <ayco.holleman@naturalis.nl>
2017-08-14 16:05:26 +0200

    record basis calculator

---
+++ nl.naturalis.nba.dao/src/main/java/nl/naturalis/nba/dao/format/calc/RecordBasisCalculator.java
@@ -0,0 +1,40 @@
+package nl.naturalis.nba.dao.format.calc;
+
+import static nl.naturalis.nba.dao.format.FormatUtil.EMPTY_STRING;
+
+import java.util.Map;
+
+import nl.naturalis.nba.api.model.Specimen;
+import nl.naturalis.nba.dao.format.CalculationException;
+import nl.naturalis.nba.dao.format.CalculatorInitializationException;
+import nl.naturalis.nba.dao.format.EntityObject;
+import nl.naturalis.nba.dao.format.ICalculator;
+
+public class RecordBasisCalculator implements ICalculator {
+
+	@Override
+	public void initialize(Map<String, String> args) throws CalculatorInitializationException
+	{
+	}
+
+	@Override
+	public Object calculateValue(EntityObject entity) throws CalculationException
+	{
+		Specimen specimen = (Specimen) entity.getDocument();
+		if (specimen.getRecordBasis() == null) {
+			return EMPTY_STRING;
+		}
+		if (specimen.getRecordBasis().contains("photo(copy) of herbarium sheet")
+				|| specimen.getRecordBasis().contains("Illustration")
+				|| specimen.getRecordBasis().contains("Photographs, negatives")
+				|| specimen.getRecordBasis().contains("DNA sample from sheet")
+				|| specimen.getRecordBasis().contains("Slides")
+				|| specimen.getRecordBasis().contains("Observation")) {
+			return EMPTY_STRING;
+		}
+		if (specimen.getRecordBasis() != null) {
+			return "PreservedSpecimen";
+		}
+		return EMPTY_STRING;
+	}
+}

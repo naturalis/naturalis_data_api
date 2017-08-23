6e2808af555a6ceb3b8e0d39310e9cfc7fb34242
Ayco Holleman <ayco.holleman@naturalis.nl>
2017-08-23 15:41:49 +0200

    remove comments

--- nl.naturalis.nba.etl/src/main/java/nl/naturalis/nba/etl/BulkIndexer.java
+++ nl.naturalis.nba.etl/src/main/java/nl/naturalis/nba/etl/BulkIndexer.java
@@ -76,8 +76,6 @@ public class BulkIndexer<T extends IDocumentObject> {
 			IndexRequestBuilder irb = client.prepareIndex(index, type);
 			try {
 				irb.setSource(om.writeValueAsBytes(documents.get(i)));
-				//DebugUtil.log("/tmp/ayco.txt", new String(om.writeValueAsBytes(documents.get(i))));
-				//System.exit(0);
 				if (ids != null) {
 					irb.setId(ids.get(i));
 				}

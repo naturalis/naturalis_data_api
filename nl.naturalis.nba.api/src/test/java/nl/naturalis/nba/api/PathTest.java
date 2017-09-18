42714c5805de3eb9770b96f7fbb4d193884be900
Tom Gilissen <tom.gilissen@naturalis.nl>
2017-09-18 15:28:51 +0200

    Unit tests for Path

--- nl.naturalis.nba.api/src/test/java/nl/naturalis/nba/api/PathTest.java
+++ nl.naturalis.nba.api/src/test/java/nl/naturalis/nba/api/PathTest.java
@@ -1,10 +1,10 @@
 package nl.naturalis.nba.api;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
-import nl.naturalis.nba.api.Path;
 
 @SuppressWarnings("static-method")
 public class PathTest {
@@ -52,5 +52,81 @@ public class PathTest {
 		path0 = path1;
 		path1 = new Path("kingdom");
 	}
+	
+	@Test
+	public void testReplace_01()
+	{
+		Path path0 = new Path("identifications.defaultClassification.kingdom");
+		Path path1 = new Path("identifications.defaultClassification.phylum");
+		assertEquals("01", path1, path0.replace(2, "phylum"));
+		Path path2 = new Path("test.defaultClassification.phylum");
+		assertEquals("02", path2, path1.replace(0, "test"));
+	}
+	
+	@Test
+	public void testCompareTo_01()
+	{
+		Path path0 = new Path("identifications.defaultClassification.kingdom");
+		Path path1 = new Path("identifications.defaultClassification.kingdom");
+		Path path2 = new Path("identifications.defaultClassification.phylum");
+		assertTrue("01", path0.compareTo(path1) == 0);
+		assertFalse("02", path0.compareTo(path2) == 0);
+	}
+	
+	@Test
+	public void testElement_01()
+	{
+		Path path0 = new Path("identifications.defaultClassification.kingdom");
+		assertEquals("01", new Path("identifications"), path0.element(0));
+		assertEquals("02", new Path("defaultClassification"), path0.element(1));
+		assertEquals("03", new Path("kingdom"), path0.element(2));
+	}
+	
+	@Test
+	public void testPath_01()
+	{
+		Path path0 = new Path("identifications.defaultClassification.kingdom");
+		Path path1 = new Path("identifications.defaultClassification.kingdom");
+		assertFalse("01", (path0 == path1));
+		assertEquals("02", path1, path0);
+	}
+	
+	@Test
+	public void testPath_02()
+	{
+		Path path0 = new Path("identifications.defaultClassification.kingdom");
+		Path path1 = new Path(path0);
+		assertFalse("01", (path0 == path1));
+		assertEquals("02", path1, path0);
+	}
+
+	
+	@Test
+	@SuppressWarnings("unlikely-arg-type")
+	public void testEquals_01()
+	{
+		Path path0 = new Path("identifications.defaultClassification.kingdom");
+		assertTrue("01", path0.equals(path0));
+		Path path1 = new Path("identifications.defaultClassification.kingdom");
+		assertTrue("02", path0.equals(path1));
+		String str = "identifications.defaultClassification.kingdom";
+		assertFalse("03", path0.equals(str));
+	}
+	
+	@Test
+	public void testHashCode()
+	{
+		Path path0 = new Path("identifications.defaultClassification.kingdom");
+		assertEquals("01", 106656136, path0.hashCode());
+	}
+	
+	@Test
+	public void testPath()
+	{
+		Path path0 = new Path();
+		Path path1 = new Path("");
+		assertFalse("01", path0.equals(path1));
+	}
+	
 
 }

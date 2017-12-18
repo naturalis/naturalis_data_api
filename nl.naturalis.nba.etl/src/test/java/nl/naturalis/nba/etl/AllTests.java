eeddb7aeec851829fc3669c5c6c833388ad61c5a
Plabon Kakoti <Plabon.kakoti@naturalis.nl>
2017-12-18 22:17:49 +0100

    Updated the class names

---
+++ nl.naturalis.nba.etl/src/test/java/nl/naturalis/nba/etl/AllTests.java
@@ -0,0 +1,16 @@
+package nl.naturalis.nba.etl;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+//@formatter:off
+@SuiteClasses({ 
+    
+    ETLUtilTest.class
+})
+//@formatter:on
+public class AllTests {
+
+}

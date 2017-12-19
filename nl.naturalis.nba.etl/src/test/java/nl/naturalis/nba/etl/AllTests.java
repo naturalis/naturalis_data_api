4fa4fe7e332dfc1a20a4d80fd5ed4192007d16e5
Plabon Kakoti <Plabon.kakoti@naturalis.nl>
2017-12-19 08:22:20 +0100

    Updated imports.

--- nl.naturalis.nba.etl/src/test/java/nl/naturalis/nba/etl/AllTests.java
+++ nl.naturalis.nba.etl/src/test/java/nl/naturalis/nba/etl/AllTests.java
@@ -3,12 +3,16 @@ package nl.naturalis.nba.etl;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import nl.naturalis.nba.etl.brahms.BrahmsImportUtilTest;
+import nl.naturalis.nba.etl.brahms.BrahmsSpecimenTransformerTest;
 
 @RunWith(Suite.class)
 //@formatter:off
 @SuiteClasses({ 
     
-    ETLUtilTest.class
+    ETLUtilTest.class,
+    BrahmsImportUtilTest.class,
+    BrahmsSpecimenTransformerTest.class
 })
 //@formatter:on
 public class AllTests {

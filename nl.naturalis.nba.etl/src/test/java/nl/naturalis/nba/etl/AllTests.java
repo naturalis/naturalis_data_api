7e1da3c8549ba990a9fb4e87b5c3966183060ba9
Plabon Kakoti <Plabon.kakoti@naturalis.nl>
2018-01-08 08:50:44 +0100

    Adding test resources.

--- nl.naturalis.nba.etl/src/test/java/nl/naturalis/nba/etl/AllTests.java
+++ nl.naturalis.nba.etl/src/test/java/nl/naturalis/nba/etl/AllTests.java
@@ -6,17 +6,40 @@ import org.junit.runners.Suite.SuiteClasses;
 import nl.naturalis.nba.etl.brahms.BrahmsImportUtilTest;
 import nl.naturalis.nba.etl.brahms.BrahmsMultiMediaTransformerTest;
 import nl.naturalis.nba.etl.brahms.BrahmsSpecimenTransformerTest;
+import nl.naturalis.nba.etl.col.CoLReferenceBatchTransformerTest;
+import nl.naturalis.nba.etl.col.CoLSynonymBatchTransformerTest;
+import nl.naturalis.nba.etl.col.CoLTaxonTransformerTest;
+import nl.naturalis.nba.etl.col.CoLVernacularNameBatchTransformerTest;
+import nl.naturalis.nba.etl.crs.CrsImportUtilTest;
+import nl.naturalis.nba.etl.crs.CrsMultiMediaTransformerTest;
+import nl.naturalis.nba.etl.crs.CrsSpecimenTransformerTest;
+import nl.naturalis.nba.etl.geo.GeoTransformerTest;
+import nl.naturalis.nba.etl.nsr.NsrImportUtilTest;
+import nl.naturalis.nba.etl.nsr.NsrMultiMediaTransformerTest;
+import nl.naturalis.nba.etl.nsr.NsrTaxonTransformerTest;
+
 
 @RunWith(Suite.class)
-//@formatter:off
 @SuiteClasses({ 
     
     ETLUtilTest.class,
     BrahmsImportUtilTest.class,
     BrahmsSpecimenTransformerTest.class,
-    BrahmsMultiMediaTransformerTest.class
+    BrahmsMultiMediaTransformerTest.class,
+    CrsSpecimenTransformerTest.class,
+    CrsImportUtilTest.class,
+    CrsSpecimenTransformerTest.class,
+    CrsMultiMediaTransformerTest.class,
+    CoLTaxonTransformerTest.class,
+    CoLSynonymBatchTransformerTest.class,
+    CoLVernacularNameBatchTransformerTest.class,
+    CoLReferenceBatchTransformerTest.class,
+    NsrTaxonTransformerTest.class,
+    NsrImportUtilTest.class,
+    NsrMultiMediaTransformerTest.class,
+    GeoTransformerTest.class,
+    TransformUtilTest.class
 })
-//@formatter:on
 public class AllTests {
 
 }

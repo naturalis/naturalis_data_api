c4b5a9c24345256e5b9373683d6227c3b34519d3
Plabon Kakoti <Plabon.kakoti@naturalis.nl>
2018-01-11 15:41:46 +0100

    Added testGenre

--- nl.naturalis.nba.etl/src/test/java/nl/naturalis/nba/etl/ETLUtilTest.java
+++ nl.naturalis.nba.etl/src/test/java/nl/naturalis/nba/etl/ETLUtilTest.java
@@ -39,6 +39,7 @@ public class ETLUtilTest {
     CoLTaxonImporter cti = new CoLTaxonImporter();
     String path = AllTests.class.getResource("taxa.txt").getPath();
     cti.importCsv(path);
+    System.setProperty("nl.naturalis.nba.etl.testGenera", "malus,parus,larus,bombus,rhododendron,felix,tulipa,rosa,canis,passer,trientalis");
   }
 
   /**

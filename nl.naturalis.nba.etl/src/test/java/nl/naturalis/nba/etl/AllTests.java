1458adb3f9b492e56ed0700c607d4cb69009a40d
Plabon Kakoti <Plabon.kakoti@naturalis.nl>
2018-01-12 10:07:17 +0100

    Added test classes to suite.

--- nl.naturalis.nba.etl/src/test/java/nl/naturalis/nba/etl/AllTests.java
+++ nl.naturalis.nba.etl/src/test/java/nl/naturalis/nba/etl/AllTests.java
@@ -8,11 +8,17 @@ import nl.naturalis.nba.etl.brahms.BrahmsMultiMediaTransformerTest;
 import nl.naturalis.nba.etl.brahms.BrahmsSpecimenTransformerTest;
 import nl.naturalis.nba.etl.col.CoLReferenceBatchTransformerTest;
 import nl.naturalis.nba.etl.col.CoLSynonymBatchTransformerTest;
+import nl.naturalis.nba.etl.col.CoLTaxonImporterTest;
 import nl.naturalis.nba.etl.col.CoLTaxonTransformerTest;
 import nl.naturalis.nba.etl.col.CoLVernacularNameBatchTransformerTest;
 import nl.naturalis.nba.etl.crs.CrsImportUtilTest;
 import nl.naturalis.nba.etl.crs.CrsMultiMediaTransformerTest;
 import nl.naturalis.nba.etl.crs.CrsSpecimenTransformerTest;
+import nl.naturalis.nba.etl.enrich.EnrichmentUtilTest;
+import nl.naturalis.nba.etl.enrich.MultimediaTaxonomicEnricher2Test;
+import nl.naturalis.nba.etl.enrich.SpecimenMultimediaEnricherTest;
+import nl.naturalis.nba.etl.enrich.SpecimenTaxonomicEnricher2Test;
+import nl.naturalis.nba.etl.geo.GeoImportUtilTest;
 import nl.naturalis.nba.etl.geo.GeoTransformerTest;
 import nl.naturalis.nba.etl.nsr.NsrImportUtilTest;
 import nl.naturalis.nba.etl.nsr.NsrMultiMediaTransformerTest;
@@ -26,19 +32,27 @@ import nl.naturalis.nba.etl.nsr.NsrTaxonTransformerTest;
     BrahmsImportUtilTest.class,
     BrahmsSpecimenTransformerTest.class,
     BrahmsMultiMediaTransformerTest.class,
-    CrsSpecimenTransformerTest.class,
     CrsImportUtilTest.class,
     CrsSpecimenTransformerTest.class,
     CrsMultiMediaTransformerTest.class,
+    CrsSpecimenTransformerTest.class,
     CoLTaxonTransformerTest.class,
+    CoLTaxonImporterTest.class,
     CoLSynonymBatchTransformerTest.class,
     CoLVernacularNameBatchTransformerTest.class,
     CoLReferenceBatchTransformerTest.class,
     GeoTransformerTest.class,
+    GeoImportUtilTest.class,
     TransformUtilTest.class,
     NsrImportUtilTest.class,
     NsrMultiMediaTransformerTest.class,
-    NsrTaxonTransformerTest.class
+    NsrTaxonTransformerTest.class,
+    EnrichmentUtilTest.class,
+    MultimediaTaxonomicEnricher2Test.class,
+    SpecimenMultimediaEnricherTest.class,
+    SpecimenTaxonomicEnricher2Test.class,
+    SummaryObjectUtilTest.class
+    
 })
 public class AllTests {
 

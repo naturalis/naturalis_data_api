ea692b9ca7c3662a1c268168281b9840a365d718
Tom Gilissen <tom.gilissen@naturalis.nl>
2018-08-03 13:45:24 +0200

    NBAX-183 #comment updated TaxonRelationType with antonyms

--- nl.naturalis.nba.api/src/main/java/nl/naturalis/nba/api/model/TaxonRelationType.java
+++ nl.naturalis.nba.api/src/main/java/nl/naturalis/nba/api/model/TaxonRelationType.java
@@ -7,14 +7,19 @@ import com.fasterxml.jackson.annotation.JsonValue;
 public enum TaxonRelationType implements INbaModelObject {
 
   HAS_FOOD_PLANT("has food plant"),
+  IS_FOOD_PLANT_OF("is food plant of"),
   HAS_HOST("has host"),
+  IS_HOST_OF("is host of"),
   HAS_HYPERPARASITE("has hyperparasite"),
+  IS_HYPERPARASITE_OF("is hyperparasite of"),
   HAS_PARASITE("has parasite"),
+  IS_PARASITE_OF("is parasite of"),
   HAS_SUBSTRATE("has substrate"),
   IS_SUBSTRATE_OF("is substrate of"),
   HAS_TRACE("has trace"),
   IS_TRACE_OF("is trace of"),
   HAS_BACKGROUND_SOUNDS("has background sounds"),
+  IS_BACKGROUND_SOUND_OF("is background sound of"),
   IN_RELATION_WITH("in relation with");
   
   private String name;

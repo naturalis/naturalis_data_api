57ebc75bb174a78e12ee6baa1b6c8df319ef6377
ayco-at-naturalis <ayco.holleman@naturalis.nl>
2018-08-13 14:32:08 +0200

    Removed system.exit from RegistryTest

--- nl.naturalis.nba.dao/src/test/java/nl/naturalis/nba/dao/RegistryTest.java
+++ nl.naturalis.nba.dao/src/test/java/nl/naturalis/nba/dao/RegistryTest.java
@@ -1,13 +1,9 @@
 package nl.naturalis.nba.dao;
 
-import static nl.naturalis.nba.dao.DocumentType.SPECIMEN;
 import static org.junit.Assert.assertNotNull;
-
 import org.apache.logging.log4j.Logger;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import nl.naturalis.nba.dao.DaoRegistry;
 import nl.naturalis.nba.utils.ConfigObject;
 
 @SuppressWarnings("static-method")
@@ -19,21 +15,6 @@ public class RegistryTest {
 	public static void init()
 	{
 		logger.info("Starting tests");
-		if (!SPECIMEN.getIndexInfo().getName().endsWith("integration_test")) {
-			/*
-			 * Unit tests are run against an ***_integration_test index.
-			 * However, some performance test are run against the nba index,
-			 * which contains real data imported by the ETL programs. For that
-			 * purpose we manually make nba-test.properties point to the real
-			 * index. Unfornately, we forgot once too often to reset
-			 * nba-test.properties before running the unit tests again, wiping
-			 * out the data in the nba index. So here some bare-knuckle way to
-			 * prevent this. Note that RegistryTest is the first of the unit
-			 * tests within the test suite.
-			 */
-			System.out.println("UPDATE nba.properties FIRST !!!!");
-			System.exit(1);
-		}
 	}
 
 	@Test

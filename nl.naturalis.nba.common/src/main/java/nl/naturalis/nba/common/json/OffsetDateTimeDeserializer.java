7d7b0e910c6466cdd1c1d345a1adfb6af2954589
Tom Gilissen <tom.gilissen@naturalis.nl>
2018-09-24 14:56:18 +0200

    Changed deserialiszer to deal with ISO_OFFSET_DATE_TIME

--- nl.naturalis.nba.common/src/main/java/nl/naturalis/nba/common/json/OffsetDateTimeDeserializer.java
+++ nl.naturalis.nba.common/src/main/java/nl/naturalis/nba/common/json/OffsetDateTimeDeserializer.java
@@ -2,7 +2,8 @@ package nl.naturalis.nba.common.json;
 
 import java.io.IOException;
 import java.time.OffsetDateTime;
-
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -15,7 +16,11 @@ class OffsetDateTimeDeserializer extends JsonDeserializer<OffsetDateTime> {
 	public OffsetDateTime deserialize(JsonParser jp, DeserializationContext ctx)
 			throws IOException, JsonProcessingException
 	{
-		return new ESDateInput(jp.getText()).parseAsOffsetDateTime(ESDateInput.ES_DATE_FORMAT);
+	  try {
+	    return OffsetDateTime.parse(jp.getText(), DateTimeFormatter.ISO_OFFSET_DATE_TIME);      // "yyyy-MM-DD'T'HH:mm:ssTZD"	    
+	  } catch (DateTimeParseException e) {
+	    return new ESDateInput(jp.getText()).parseAsOffsetDateTime(ESDateInput.ES_DATE_FORMAT); // "yyyy-MM-dd'T'HH:mm:ssZ"
+	  }
 	}
 
 }

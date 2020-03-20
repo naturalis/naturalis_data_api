b57d7587449fcad64af94bb964adde7fac2031b0
Tom Gilissen <t.gilissen@gmail.com>
2020-03-20 13:48:23 +0100

    NBAX-304 #comment modified update script #resolve

--- docker/api/update_dwca_config.sh
+++ docker/api/update_dwca_config.sh
@@ -7,15 +7,19 @@ fi
 echo "Updating branch $BRANCH"
 
 cd /etc/nba/dwca || exit 1
+
 git pull origin $BRANCH
 if [ "$?" != 0 ]; then
         echo "Unknown branch name: $BRANCH. Please use an existing branch name"
-        echo "No files were updated!"
+        echo "DWCA config was not updated!"
         exit 1
 fi
 git checkout $BRANCH
 if [ "$?" != 0 ]; then
         echo "Checkout of branch failed. Please retry"
-        echo "No files were updated!"
+        echo "DWCA config was not updated!"
         exit 1
 fi
+
+echo "DWCA config was updated with the latest settings from branch $BRANCH"
+exit 0
